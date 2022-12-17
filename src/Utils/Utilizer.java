bc1b9a231da583aec85207053cb177f5bb6ef5a8
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2022-12-18 00:23:55 +0100

    Optimized the Utilizer class, for DRY purposes

---
+++ src/Utils/Utilizer.java
@@ -0,0 +1,34 @@
+package Utils;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Utilizer {
+    private static Locale locale = new Locale.Builder().setLanguage("nl").setRegion("NL").build();
+    private static NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+
+
+    /**
+     * Biedt de mogelijkheid om op basis van invoer van bedrag en
+     * (optioneel) precisie een valuta bedrag in euro's terug te
+     * ontvangen, en met precisie het aantal decimalen te bepalen.
+     * @param bedrag voer een double waarden is
+     * @return valuta in euro's
+     */
+    public static String euroBedrag(double bedrag) {
+        return currencyFormatter.format(bedrag);
+    }
+
+    /**
+     * Biedt de mogelijkheid om op basis van invoer van bedrag en
+     * (optioneel) precisie een valuta bedrag in euro's terug te
+     * ontvangen, en met precisie het aantal decimalen te bepalen.
+     * @param bedrag voer een double waarden is
+     * @param precisie voer int waarden om de decimalen te bepalen.
+     * @return valuta in euro's
+     */
+    public static String euroBedrag(double bedrag, int precisie) {
+        currencyFormatter.setMaximumFractionDigits(precisie);
+        return currencyFormatter.format(bedrag);
+    }
+}
